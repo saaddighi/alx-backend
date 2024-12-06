ada19860df93201a38941f178acd1d114f6a2112
saaddighi <saaddighi20@gmail.com>
2024-12-06 17:24:09 +0000

    ok

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+        
+
+import csv
+import math
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """a function that eturn a tuple of size two containing a
+    start index and an end index corresponding
+    to the range of indexes to return in a list
+    for those particular pagination parameters"""
+
+    indexa = 0
+    indexf = 0
+    if page == 1:
+        indexa = 0
+        indexf = page_size
+    else:
+        indexa = (page - 1) * page_size
+        indexf = page * page_size
+    return (indexa, indexf)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
+            a , b = index_range(page, page_size)
+            lst = []
+            self.dataset[a:b].append(lst)
+            return lst
