b45012d554c8291eae748ce35e813f11f0cd9e9c
saaddighi <saaddighi20@gmail.com>
2024-12-10 11:04:17 +0000

    ok

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-    
-BaseCaching = __import__('base_caching').BaseCaching    
+""" BasicCache module
+"""    
+
+BaseCaching = __import__('base_caching').BaseCaching
+""" importing the parent class
+"""    
 
 
 class BasicCache(BaseCaching):
-    
+    """ BasicCache defines:
+      - method to update the dict
+      - method to check if key in in the dict
+    """
     def __init__(self):
+        """ Initiliaze
+        """
         super().__init__()
         dic = self.cache_data
     
     
     def put(self, key, item):
+        """ return updated dict
+        """
         if key == None or item == None:
             pass
         else:
             return self.cache_data.update({key: item})
     def get(self, key):
-        
+        """ check dict for specific key
+        """
         if key == None or key not in self.cache_data:
             return None
         else:
             return self.cache_data.get('key')
-            
-            
