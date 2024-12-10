849c11c15d1b44c404b51567ed29bbfc577b1545
saaddighi <saaddighi20@gmail.com>
2024-12-10 17:22:48 +0000

    ok

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,7 +5,8 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    
+    """ FIFO caching method 
+        """
     def __init__(self):
         super().__init__()
     
@@ -16,6 +17,8 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
             
         while len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            """ deleting first item off the dict
+            """
             lst = list(self.cache_data.keys())
             print(lst[0])
             del self.cache_data[lst[0]]
