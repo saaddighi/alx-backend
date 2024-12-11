86257faed940cb204819c4db79206b2ed2a656df
saaddighi <saaddighi20@gmail.com>
2024-12-11 11:25:26 +0000

    ok

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,17 +8,17 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFO caching method 
+    """ FIFO caching method
         """
     def __init__(self):
         super().__init__()
-    
+
     def put(self, key, item):
-        """ Assign the item to the dictionary 
+        """ Assign the item to the dictionary
         """
         if key is None or item is None:
             return
-            
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             """ deleting first item off the dict
             """
@@ -26,7 +26,7 @@ class FIFOCache(BaseCaching):
             self.cache_data.pop(first_key)
             print(f'DISCARD: {first_key}')
         self.cache_data[key] = item
-            
+
     def get(self, key):
         """ Get an item from the cache by its key """
         if key is None or key not in self.cache_data:
