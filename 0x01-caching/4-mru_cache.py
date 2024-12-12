ef4e2b1aed127335d1c3f906f702940b611b8eed
saaddighi <saaddighi20@gmail.com>
2024-12-12 18:11:16 +0000

    ok

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -25,7 +25,7 @@ class MRUCache(BaseCaching):
             return
         
         if key in self.cache_data:
-            self.cache_datat.move_to_end(key)
+            self.cache_data.move_to_end(key)
 
         self.cache_data[key] = item
         
