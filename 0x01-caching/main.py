02bc0c1df5f6a5516c8630816d93e66e67f61794
saaddighi <saaddighi20@gmail.com>
2024-12-12 11:31:19 +0000

    ok

--- 0x01-caching/main.py
+++ 0x01-caching/main.py
@@ -7,5 +7,9 @@ try:
     my_cache.print_cache()
     my_cache.put("test1", "myValue")
     my_cache.print_cache()
+    my_cache.put(None, "myValue")
+    my_cache.print_cache()
+    my_cache.put("test1", None)
+    my_cache.print_cache()
 except:
     print(sys.exc_info()[1])
