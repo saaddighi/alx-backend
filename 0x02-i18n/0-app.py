97db4bfa73c56f5114a8af2f6aeedaff8edf6211
saaddighi <saaddighi20@gmail.com>
2024-12-18 11:25:57 +0000

    ok

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 
 from flask import Flask, render_template
 
@@ -8,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
      return render_template('0-index.html')
- 
- 
+
+
 if __name__ == "__main__":
     app.run(debug=True)
