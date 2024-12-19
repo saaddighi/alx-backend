ca5bdd639387c0d2a1ab53d9c57e559fbb426063
saaddighi <saaddighi20@gmail.com>
2024-12-19 12:11:23 +0000

    ok

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+
+"""task 1 creat a flask app"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import gettext as _
+
+
+class Config:
+    """config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """geting best language match"""
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """'/' route"""
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
