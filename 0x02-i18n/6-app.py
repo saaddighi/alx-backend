7a0b2d54c641aa5148d21c12ea9eee3e8e62b420
saaddighi <saaddighi20@gmail.com>
2024-12-23 16:29:24 +0000

    ok

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,65 @@
+
+"""task 1 creat a flask app"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from flask_babel import gettext as _
+
+
+class Config:
+    """config class """
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """gets user based on id"""
+    userid = request.args.get('login_as')
+    if userid:
+        return users.get(int(userid))
+    return None
+
+
+@app.before_request
+def before_request():
+    """to do before request"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """geting best language match"""
+    locale = request.args.get('locale')
+    acc_local = request.headers.get('Accept-Language')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    elif locale is None:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    elif acc_local and acc_local is not None:
+        return acc_local
+    else:
+        return Config.BABEL_DEFAULT_LOCALE
+
+@app.route('/')
+def home():
+    """'/'route"""
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
